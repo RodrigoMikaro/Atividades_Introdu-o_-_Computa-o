506fc12023584ff3728d34fdeb969e700fc8d17b
Rodrigo Mikaro <rodrigopettinati2007@outlook.com.br>
2026-03-31 20:31:05 -0300

    EP1 parece pronto

--- EP1/ep1.c
+++ EP1/ep1.c
@@ -30,44 +30,97 @@
 
 // --------------- PROTÓTIPOS --------------- //
 
-    int update_best_time(int pilot, int attempt, int attempt_time);
+    int best_to_s_best();
+    int update_best();
+    int update_s_best();
+    int get_best_time();
+    int get_s_best_time();
+    int print_first_place();
+    int print_second_place();
 
-    int update_podium() 
 
 
 // --------------- VARIÁVEIS GLOBAIS --------------- //
 
     // Argumentos que serão lidos na entrada padrão:
     // (nº piloto) , (nº tentativa), (tempo da tentativa)
-        int pilot, attempt, attempt_time = 0;
+        int pilot, attempt, time = 0;
 
-    // Melhores tempos de cada piloto:
-        int best_1, best_2, best_3, best_4, best_5, best_6, best_7, best_8, best_9, best_10 = 100000000;
-        int best_11, best_12, best_13, best_14, best_15, best_16, best_17, best_18, best_19, best_20 = 100000000;
-
-    // Tentativa na qual cada piloto teve seu melhor tempo:
-        int attempt_1, attempt_2, attempt_3, attempt_4, attempt_5, attempt_6, attempt_7, attempt_8, attempt_9, attempt_10;
-        int attempt_11, attempt_12, attempt_13, attempt_14, attempt_15, attempt_16, attempt_17, attempt_18, attempt_19, attempt_20;
-
-        int best_time, best_attempt, best_pilot;
-        int second_best_time, second_best_pilot;
+    // Variáveis armazenando primeiro e segundo melhores:
+        int best_pilot = 21;
+        int s_best_pilot = 21;
+        int best_attempt = 9999999;
+        int s_best_attempt = 9999999;
+        int best_time = 9999999;
+        int s_best_time = 9999999;
+    
+    // Variáveis de tempo:
+        int best_time_min;
+        int best_time_sec;
+        int best_time_mil;
+        int s_best_time_min;
+        int s_best_time_sec;
+        int s_best_time_mil;
 
 
 
 // --------------- MAIN --------------- //
     
-    int main() {
-        scanf("%d %d %d", &pilot, &attempt, &attempt_time);
-        best_pilot
-        best_time = 
-        best_attempt
-        
+    int main(){
+        scanf("%d %d %d", &pilot, &attempt, &time);
+        best_pilot = pilot;
+        best_attempt = attempt;
+        best_time = time;
 
-        while (scanf("%d %d %d", &pilot, &attempt, &attempt_time) != EOF) {
-            update_best_time(pilot, attempt, attempt_time);
+        while (scanf("%d %d %d", &pilot, &attempt, &time) != EOF) {
+            
+            // Verifica se é o best:
+                if (time < best_time) {
+                    best_to_s_best();
+                    update_best();
+                }
+
+                if (time == best_time) {
+                    if (pilot != best_pilot) {
+                        if (attempt < best_attempt) {
+                            best_to_s_best();
+                            update_best();
+                        }
+                        else if (attempt == best_attempt) {
+                            if (pilot < best_pilot) {
+                                best_to_s_best();
+                                update_best();
+                            }
+                        }
+                    }
+                }
+
+            // Verifica se é o s_best:
+                if (pilot != best_pilot) {
+                    if (time < s_best_time) {
+                        update_s_best();
+                    }
+                    else if (time == s_best_time) {
+                        if (pilot != s_best_pilot) {
+                            if (attempt < s_best_attempt) {
+                                update_s_best();
+                            }
+                            else if (attempt == s_best_attempt) {
+                                if (pilot < s_best_pilot) {
+                                    update_s_best();
+                                }
+                            }
+                        }
+                    }
+                }
         }
 
-        printf("")
+        get_best_time();
+        get_s_best_time();
+
+        print_first_place();
+        print_second_place();
+
         return 0;
     }
 
@@ -75,229 +128,95 @@
 
 // --------------- FUNÇÕES --------------- //
 
-    int update_best_time(int pilot, int attempt, int attempt_time) {
-        
-        if (pilot == 1) {
-            if (attempt == 1) {
-                best_1 = attempt_time;
-                attempt_1 = 1;
-            }
-            else if (attempt_time < best_1) {
-                best_1 = attempt_time;
-                attempt_1 = attempt;
-            }
-        }
 
-        if (pilot == 2) {
-            if (attempt == 1) {
-                best_2 = attempt_time;
-                attempt_2 = 1;
-            }
-            else if (attempt_time < best_2) {
-                best_2 = attempt_time;
-                attempt_2 = attempt;
-            }
+    int best_to_s_best(){
+        if (pilot != best_pilot) {
+            s_best_pilot = best_pilot;
+            s_best_attempt = best_attempt;
+            s_best_time = best_time;
         }
-
-        if (pilot == 3) {
-            if (attempt == 1) {
-                best_3 = attempt_time;
-                attempt_3 = 1;
-            }
-            else if (attempt_time < best_3) {
-                best_3 = attempt_time;
-                attempt_3 = attempt;
-            }
-        }
-
-        if (pilot == 4) {
-            if (attempt == 1) {
-                best_4 = attempt_time;
-                attempt_4 = 1;
-            }
-            else if (attempt_time < best_4) {
-                best_4 = attempt_time;
-                attempt_4 = attempt;
-            }
-        }
-
-        if (pilot == 5) {
-            if (attempt == 1) {
-                best_5 = attempt_time;
-                attempt_5 = 1;
-            }
-            else if (attempt_time < best_5) {
-                best_5 = attempt_time;
-                attempt_5 = attempt;
-            }
-        }
-
-        if (pilot == 6) {
-            if (attempt == 1) {
-                best_6 = attempt_time;
-                attempt_6 = 1;
-            }
-            else if (attempt_time < best_6) {
-                best_6 = attempt_time;
-                attempt_6 = attempt;
-            }
-        }
-
-        if (pilot == 7) {
-            if (attempt == 1) {
-                best_7 = attempt_time;
-                attempt_7 = 1;
-            }
-            else if (attempt_time < best_7) {
-                best_7 = attempt_time;
-                attempt_7 = attempt;
-            }
-        }
-    
-        if (pilot == 8) {
-            if (attempt == 1) {
-                best_8 = attempt_time;
-                attempt_8 = 1;
-            }
-            else if (attempt_time < best_8) {
-                best_8 = attempt_time;
-                attempt_8 = attempt;
-            }
-        }
-
-        if (pilot == 9) {
-            if (attempt == 1) {
-                best_9 = attempt_time;
-                attempt_9 = 1;
-            }
-            else if (attempt_time < best_9) {
-                best_9 = attempt_time;
-                attempt_9 = attempt;
-            }
-        }
-
-        if (pilot == 10) {
-            if (attempt == 1) {
-                best_10 = attempt_time;
-                attempt_10 = 1;
-            }
-            else if (attempt_time < best_10) {
-                best_10 = attempt_time;
-                attempt_10 = attempt;
-            }
-        }
-
-        if (pilot == 11) {
-            if (attempt == 1) {
-                best_11 = attempt_time;
-                attempt_11 = 1;
-            }
-            else if (attempt_time < best_11) {
-                best_11 = attempt_time;
-                attempt_11 = attempt;
-            }
-        }
-
-        if (pilot == 12) {
-            if (attempt == 1) {
-                best_12 = attempt_time;
-                attempt_12 = 1;
-            }
-            else if (attempt_time < best_12) {
-                best_12 = attempt_time;
-                attempt_12 = attempt;
-            }
-        }
-
-        if (pilot == 13) {
-            if (attempt == 1) {
-                best_13 = attempt_time;
-                attempt_13 = 1;
-            }
-            else if (attempt_time < best_13) {
-                best_13 = attempt_time;
-                attempt_13 = attempt;
-            }
-        }
-
-        if (pilot == 14) {
-            if (attempt == 1) {
-                best_14 = attempt_time;
-                attempt_14 = 1;
-            }
-            else if (attempt_time < best_14) {
-                best_14 = attempt_time;
-                attempt_14 = attempt;
-            }
-        }
-
-        if (pilot == 15) {
-            if (attempt == 1) {
-                best_15 = attempt_time;
-                attempt_15 = 1;
-            }
-            else if (attempt_time < best_15) {
-                best_15 = attempt_time;
-                attempt_15 = attempt;
-            }
-        }
-
-        if (pilot == 16) {
-            if (attempt == 1) {
-                best_16 = attempt_time;
-                attempt_16 = 1;
-            }
-            else if (attempt_time < best_16) {
-                best_16 = attempt_time;
-                attempt_16 = attempt;
-            }
-        }
-
-        if (pilot == 17) {
-            if (attempt == 1) {
-                best_17 = attempt_time;
-                attempt_17 = 1;
-            }
-            else if (attempt_time < best_17) {
-                best_17 = attempt_time;
-                attempt_17 = attempt;
-            }
-        }
-    
-        if (pilot == 18) {
-            if (attempt == 1) {
-                best_18 = attempt_time;
-                attempt_18 = 1;
-            }
-            else if (attempt_time < best_18) {
-                best_18 = attempt_time;
-                attempt_18 = attempt;
-            }
-        }
-
-        if (pilot == 19) {
-            if (attempt == 1) {
-                best_19 = attempt_time;
-                attempt_19 = 1;
-            }
-            else if (attempt_time < best_19) {
-                best_19 = attempt_time;
-                attempt_19 = attempt;
-            }
-        }
-
-        if (pilot == 20) {
-            if (attempt == 1) {
-                best_20 = attempt_time;
-                attempt_20 = 1;
-            }
-            else if (attempt_time < best_20) {
-                best_20 = attempt_time;
-                attempt_20 = attempt;
-            }
-        }
-
-    best_time = attempt_time
-
+        return 0;
     }
 
+    int update_best(){
+        best_pilot = pilot;
+        best_attempt = attempt;
+        best_time = time;
+    }
+
+    int update_s_best(){
+        s_best_pilot = pilot;
+        s_best_attempt = attempt;
+        s_best_time = time;
+    }
+
+    int get_best_time(){
+        best_time_min = best_time / 60000;
+        best_time = best_time - best_time_min * 60000;
+        best_time_sec = best_time / 1000;
+        best_time = best_time - best_time_sec * 1000;
+        best_time_mil = best_time;
+    }
+
+    int get_s_best_time(){
+        s_best_time_min = s_best_time / 60000;
+        s_best_time = s_best_time - s_best_time_min * 60000;
+        s_best_time_sec = s_best_time / 1000;
+        s_best_time = s_best_time - s_best_time_sec * 1000;
+        s_best_time_mil = s_best_time;
+    }
+
+    int print_first_place(){
+        printf("Pole position: Piloto %d", best_pilot);
+
+        if (best_time_min != 1) {
+            printf(", com tempo de %d minutos, ", best_time_min);
+        }
+        else if (best_time == 1) {
+            printf(", com tempo de 1 minuto, ");
+        }
+
+        if (best_time_sec != 1) {
+            printf("%d segundos e", best_time_sec);
+        }
+        else if (best_time_sec == 1) {
+            printf("1 segundo e");
+        }
+
+        if (best_time_mil != 1) {
+            printf(" %d milissegundos", best_time_mil);
+        }
+        else if (best_time_mil == 1) {
+            printf(" 1 milissegundo");
+        }
+
+        printf(" (tentativa %d).\n", best_attempt);
+    }
+
+    
+    int print_second_place(){
+        printf("Segundo lugar: Piloto %d", s_best_pilot);
+
+        if (s_best_time_min != 1) {
+            printf(", com tempo de %d minutos, ", s_best_time_min);
+        }
+        else if (s_best_time == 1) {
+            printf(", com tempo de 1 minuto, ");
+        }
+
+        if (s_best_time_sec != 1) {
+            printf("%d segundos e", s_best_time_sec);
+        }
+        else if (s_best_time_sec == 1) {
+            printf("1 segundo e");
+        }
+
+        if (s_best_time_mil != 1) {
+            printf(" %d milissegundos", s_best_time_mil);
+        }
+        else if (s_best_time_mil == 1) {
+            printf(" 1 milissegundo");
+        }
+
+        printf(" (tentativa %d).\n", s_best_attempt);
+    }
