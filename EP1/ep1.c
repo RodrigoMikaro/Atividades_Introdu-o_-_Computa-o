c174dd49f3637f6f6dd6c80357353de05332cde0
Rodrigo Mikaro <rodrigopettinati2007@outlook.com.br>
2026-03-31 14:27:38 -0300

    início do ep1

---
+++ EP1/ep1.c
@@ -0,0 +1,49 @@
+/******************************************************************************
+ Ao preencher esse cabeçalho com o meu nome e o meu número USP, declaro que sou
+ o único autor e responsável por esse programa. Todas as partes originais desse
+ Exercício-Programa (EP) foram desenvolvidas e implementadas por mim seguindo
+ as instruções desse EP e que portanto não constituem desonestidade acadêmica
+ ou plágio.
+
+ Declaro também que sou responsável por todas as cópias desse programa e que eu
+ não distribuí ou facilitei a sua distribuição. Estou ciente que os casos de
+ plágio e desonestidade acadêmica serão tratados segundo os critérios
+ divulgados na página da disciplina.
+
+ Entendo que EPs sem assinatura não serão corrigidos e, ainda assim, poderão
+ ser punidos por desonestidade acadêmica.
+
+ Nome :
+ NUSP :
+ Turma:
+ Prof.: 
+******************************************************************************/
+
+
+
+// --------------- BIBLIOTECAS --------------- //
+
+    #include <stdio.h>
+    #include <stdlib.h>
+
+
+
+// --------------- PROTÓTIPOS --------------- //
+
+
+
+
+
+// --------------- MAIN --------------- //
+    
+    int main() {
+        
+    
+    return 0;
+  }
+
+
+
+// --------------- FUNÇÕES --------------- //
+
+int 
